274f766d4eddb2b0eb0fad8cfcce1c7c33f86259
Shivam Shekhar <samshekhar14@gmail.com>
2024-01-16 00:03:51 +0530

    Fix db connection

--- src/db.ts
+++ src/db.ts
@@ -3,13 +3,13 @@ import { Pool } from "pg";
 let _pool: any;
 export const initDB = () => {
   if (_pool) {
-    alert('DB instance already exists.')
+    alert("DB instance already exists.");
     return;
   }
   _pool = new Pool({
     user: process.env.DB_USER || "me",
     host: process.env.DB_HOST || "localhost",
-    database: process.env.DB_URL || "api",
+    database: process.env.DB_NAME || "api",
     password: process.env.DB_PASSWORD || "password",
     port: 5432,
   });
--- src/routes/healthCheck.routes.ts
+++ src/routes/healthCheck.routes.ts
@@ -13,4 +13,19 @@ router.get(
   }
 );
 
+router.get(
+  "/health/env",
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      res.status(200).json({
+        db_host: process.env.DB_HOST,
+        db_user: process.env.DB_USER,
+        db_url: process.env.DB_URL,
+        db_name: process.env.DB_NAME,
+      });
+    } catch (error) {
+      next(error);
+    }
+  }
+);
 export default router;
